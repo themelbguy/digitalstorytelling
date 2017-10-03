f4b71f7fcc7c4132a4474913e90437318da5c262
themelbguy <31976645+themelbguy@users.noreply.github.com>
2017-10-03 11:17:29 +1100

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+Filename: head.php
+Author: Abdul Wasay, Alessandro Gozali, Florence Chau, Kongsakul Jaijaroen
+Date Created: 06 September 2017
+Last Updated:
+Desrciption: Head section of Department of Screen and Media Digital Storytelling Website
+*/
+
+// include the myFunctions file
+include "includes/myFunctions.php";
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<!-- document's character set -->
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="desrciption" content="Department of Screen and Media Digital Storytelling Website">
+	<meta name="keywords" content="swinburne, university, screen, media, digital, storytelling, dst, digitalstorytelling">
+	<meta name="author" content="Abdul Wasay, Alessandro Gozali, Florence Chau, Kongsakul Jaijaroen">
+
+	<title>Digital Storytelling</title>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <srcipt src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></srcipt>
+      <srcipt src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></srcipt>
+    <![endif]-->
+
+		<link rel="stylesheet" href="css/styles.css" media="screen">
+
+	  <link rel="stylesheet" href="css/bootstrap-theme.css" media="screen">
+
+	  <link rel="stylesheet" href="css/bootstrap-theme.min.css" media="screen">
+
+	  <link rel="stylesheet" href="css/bootstrap.css" media="screen">
+
+	  <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+
+	<!-- Bootstrap recommends including these at the end of the page  -->
+    <!-- jQuery (necessary for Bootstrap's Javasrcipt plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+	<!-- link to required Bootstrap JS files -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+	<!-- link to external JavaScript file -->
+	<script type="text/javascript" src="js/scripts.js"></script>
+
+	<!-- link to google fonts -->
+
+	<!-- link to ScrollMagic -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+</head>
