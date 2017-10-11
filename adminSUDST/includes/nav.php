4f588a7ffad49e8f576a98a17f9b477bfe2164f9
themelbguy <31976645+themelbguy@users.noreply.github.com>
2017-10-11 11:05:34 +1100

    Add files via upload

--- adminSUDST/includes/nav.php
+++ adminSUDST/includes/nav.php
@@ -14,9 +14,40 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="">ADD</a></li>
-        <li><a href="">UPDATE</a></li>
-        <li><a href="">REMOVE</a></li>
+
+        <li><div class="dropdown"></li>
+          <button class="dropbtn" onclick="myFunction()">ADD
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content" id="myDropdown">
+            <a href="#">Admin User</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+
+        <li><div class="dropdown"></li>
+          <button class="dropbtn" onclick="myFunction()">UPDATE
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content" id="myDropdown">
+            <a href="#">Social Media</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+
+        <li><div class="dropdown"></li>
+          <button class="dropbtn" onclick="myFunction()">REMOVE
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content" id="myDropdown">
+            <a href="#">Project</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+
         <li><a href="">CHANGE PASSWORD</a></li>
         <li><a href="adminLogin.php">LOGOUT</a></li>
       </ul>
